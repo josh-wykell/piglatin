186cb98778eb5f6bceb2c6a6d4dfd6b06c70c7b3
Josh Wykell <jwykell@gmail.com>
2015-05-26 20:30:36 -0400

    works for single words only

--- piglatin.rb
+++ piglatin.rb
@@ -1,4 +1,22 @@
 class Piglatin
 
-  def initialize
+  def initialize(word)
+    @word = word
   end
+
+  def translate
+    if %w(a e i o u).include?(first_letter)
+      @word + "ay"
+    else
+      vowel_index = /[aeiou]/ =~ @word
+      @word[vowel_index..-1] + @word[0..vowel_index-1] +"ay"
+                          #[0...vowel index] does the same thing
+    end
+  end
+
+  def first_letter
+    @word[0]
+  end
+
+end
+
--- piglatin_test.rb
+++ piglatin_test.rb
@@ -2,20 +2,34 @@ require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-require 'piglatin'
+require './piglatin'
 
-class PiglatinTest < Minitest::PiglatinTest
+class PiglatinTest < Minitest::Test
 
   def setup
-    Piglatin.new
   end
 
-  def test_01_human_is_umanhay
+  def test_humans_are_umanhay
+    human = Piglatin.new("human")
     expected = "umanhay"
-    assert_equal expected, Piglatin.translate "hello"
+    assert_equal expected, human.translate
   end
 
+  def test_vowels_end_with_ay
+    occupying = Piglatin.new("occupying")
+    expected = "occupyingay"
+    assert_equal expected, occupying.translate
+  end
 
+  def test_special_rule_for_words_begining_with_t
+    transformation = Piglatin.new("transformation")
+    assert_equal "ansformationtray", transformation.translate
+  end
+
+  def test_if_multiple_words_are_piglatin
+    combat_complains = Piglatin.new("combat complains")
+    assert_equal "ombatcay omplainscay", combat_complains.translate
+  end
 
   def self.test_order
     :alpha
