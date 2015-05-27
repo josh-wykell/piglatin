a7fae05faed9c3546ee4653eea5f1c5cdb7156cb
Josh Wykell <jwykell@gmail.com>
2015-05-27 07:33:48 -0400

    working on multiple word phrases

--- piglatin.rb
+++ piglatin.rb
@@ -5,18 +5,21 @@ class Piglatin
   end
 
   def translate
-    if %w(a e i o u).include?(first_letter)
-      @word + "ay"
-    else
-      vowel_index = /[aeiou]/ =~ @word
-      @word[vowel_index..-1] + @word[0..vowel_index-1] +"ay"
-                          #[0...vowel index] does the same thing
+    phrase = @word.split.map do |word|
+      if %w(a e i o u).include?(word[0])
+        word + "ay"
+      else
+        vowel_index = /[aeiou]/ =~ word
+        word[vowel_index..-1] + word[0..vowel_index-1] +"ay"
+                            #[0...vowel index] does the same thing
+      end
     end
+    @word = phrase.join(" ")
   end
 
-  def first_letter
-    @word[0]
-  end
+  # def first_letter
+  #   @word[0]
+  # end
 
 end
 
